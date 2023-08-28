9ee7018a480c22ae9f8c66ff37c89f2430a596c0
coolwater91 <dksemrrbs@gmail.com>
2023-08-28 18:31:39 +0900

    1

---
+++ shopp/src/main/java/com/shopp/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.shopp.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ shopp/src/test/java/com/shopp/ShoppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
